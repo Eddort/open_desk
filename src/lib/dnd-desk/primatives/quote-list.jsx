282fe9b2a1c2299f92e5eea5e26cc49745370086
Eddort <webisforce@gmail.com>
2018-04-09 15:54:43 +0300

    Добавил рабочую версию npm link

--- src/lib/dnd-desk/primatives/quote-list.jsx
+++ src/lib/dnd-desk/primatives/quote-list.jsx
@@ -76,7 +76,7 @@ class InnerQuoteList extends Component<QuoteListProps> {
     return (
       <div>
         {this.props.quotes.map((quote: Quote, index: number) => (
-          <Draggable isDragDisabled={ true } key={quote.id} draggableId={quote.id} index={index}>
+          <Draggable isDragDisabled={ false } key={quote.id} draggableId={quote.id} index={index}>
             {(dragProvided: DraggableProvided, dragSnapshot: DraggableStateSnapshot) => (
               <div>
                 <QuoteItem
